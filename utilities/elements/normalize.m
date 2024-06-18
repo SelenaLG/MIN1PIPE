91fe3509806d23bec17de2883dcd326974a219ef
SelenaLG <157131127+SelenaLG@users.noreply.github.com>
2024-06-18 13:53:31 +0200

    [FIX] Update normalize.m
    Change nanmin and nanmax to min and max (recommended by MATLAB)

--- utilities/elements/normalize.m
+++ utilities/elements/normalize.m
@@ -8,12 +8,12 @@ function norm_frame = normalize(frame_in, dim)
         dim = 4; %%% treat the tensor as a whole %%%
     end
     if dim == 4
-        mn = nanmin(nanmin(nanmin(frame_in)));
+        mn = min(min(min(frame_in)));
         norm_frame = frame_in - mn;
-        mx = nanmax(nanmax(nanmax(norm_frame)));
+        mx = max(max(max(norm_frame)));
         norm_frame = norm_frame / mx;
     else
-        norm_frame = frame_in - repmat(nanmin(frame_in, [], dim), 1, size(frame_in, dim));
-        norm_frame = norm_frame ./ (repmat(nanmax(norm_frame, [], dim), 1, size(frame_in, dim)));
+        norm_frame = frame_in - repmat(min(frame_in, [], dim), 1, size(frame_in, dim));
+        norm_frame = norm_frame ./ (repmat(max(norm_frame, [], dim), 1, size(frame_in, dim)));
     end
 end
